ba0b0f9d996bed77fa1cde544ccfe1b417ab270f
Silvio <vinhoterranossa@hotmail.com>
2024-06-14 20:42:10 -0300

    Feat: Iniciando a construção de entidades

--- src/main/java/com/gestaodetarefas/gestao_de_tarefas/task/TaskEntity.java
+++ src/main/java/com/gestaodetarefas/gestao_de_tarefas/task/TaskEntity.java
@@ -1,51 +1,67 @@
 package com.gestaodetarefas.gestao_de_tarefas.task;
 
-import java.util.UUID;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
+@Entity(name = "task")
 public class TaskEntity {
-    // private UUID id;
-    private int id;
-    private String title;
-    private String status;
-    private String description;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-    public TaskEntity(int id, String title, String status, String description){
-        this.id = id;
-        this.title = title;
-        this.status = status;
-        this.description = description;
-    }
-
-    // Getters e Setters
-
-    public int getId() {
-        return id;
-    }
-    
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
+    @Column(nullable = false)
+    private String name;
 }
+
+
+// public class TaskEntity {
+//     // private UUID id;
+//     private int id;
+//     private String title;
+//     private String status;
+//     private String description;
+
+
+//     public TaskEntity(int id, String title, String status, String description){
+//         this.id = id;
+//         this.title = title;
+//         this.status = status;
+//         this.description = description;
+//     }
+
+//     // Getters e Setters
+
+//     public int getId() {
+//         return id;
+//     }
+
+//     public String getTitle() {
+//         return title;
+//     }
+
+//     public void setTitle(String title) {
+//         this.title = title;
+//     }
+
+//     public String getDescription() {
+//         return description;
+//     }
+
+//     public void setDescription(String description) {
+//         this.description = description;
+//     }
+
+//     public String getStatus() {
+//         return status;
+//     }
+
+//     public void setStatus(String status) {
+//         this.status = status;
+//     }
+// }
 
 
 // id: Chave primária da entidade.
